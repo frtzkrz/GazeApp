0f33924b8a19af4fbb2e40a9482f85c97459b6c1
frtzkrz <friedrichkaspargiuliano.kurz@uzh.ch>
2025-11-28 15:42:36 +0100

    remove h5 files

--- GazeOptimizer/optimizer_functions/gaze_angle.py
+++
@@ -1,7 +0,0 @@
-def calculate_gaze_angle(self, gaze_angle):
-    """
-    To be called if new gaze angle has to be examined. Calculates Cost, Metrics, DVHs, Dose
-    """
-    self.expensive_cost(gaze_angle=gaze_angle)
-
-
